11c95312b39646a460ab61d409cbf8690d9233b8
amjadz <amjadz@uw.edu>
2018-05-22 20:03:27 -0700

    Got api working

--- twitter.R
+++ twitter.R
@@ -1,14 +1,18 @@
 library(httr)
 library(jsonlite)
+library(twitteR)
 
 source("api-keys.R")
 
-address <- "10309 24th Ave SW"
+#address <- "10309 24th Ave SW"
 
-base_uri <- "https://api.twitter.com/1.1"
-endpoint <- "/search/tweets.json"
-query_params <- "/oauth2/token"
+#base_uri <- "https://api.twitter.com/1.1"
+#endpoint <- "/search/tweets.json"
+#query_params <- "/oauth2/token"
 
-twitter_response <- GET(paste0(base_uri, endpoint))
-twitter_content <- content(twitter_response, "text")
-twitter_data <- fromJSON(twitter_content)
+#twitter_response <- GET(paste0(base_uri, endpoint))
+#twitter_content <- content(twitter_response, "text")
+#twitter_data <- fromJSON(twitter_content)
+
+
+setup_twitter_oauth(consumer_public, consumer_private, public_key, private_key)
