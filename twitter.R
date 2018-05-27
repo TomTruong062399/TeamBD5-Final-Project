a19c8c17f014b38bdbd457e88f3a47fb840a02f2
amjadz <amjadz@uw.edu>
2018-05-27 14:49:55 -0700

    Pulled up tweets

--- twitter.R
+++ twitter.R
@@ -6,6 +6,6 @@ source("api-keys.R")
 
 setup_twitter_oauth(consumer_public, consumer_private, public_key, private_key)
 
-conservative_tweets <- searchTwitter("#Conservatives", n = 100) 
-liberal_tweets <- searchTwitter("#Liberals", n = 100)
+conservative_tweets <- searchTwitter("#Conservatives") 
+liberal_tweets <- searchTwitter("#Liberals")
 
