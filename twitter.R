7af12d8ee8387c5420db663c511938fecfd0fee2
amjadz <amjadz@uw.edu>
2018-05-27 14:47:00 -0700

    Got liberal and Conservative tweets

--- twitter.R
+++ twitter.R
@@ -5,4 +5,7 @@ library(twitteR)
 source("api-keys.R")
 
 setup_twitter_oauth(consumer_public, consumer_private, public_key, private_key)
-tweets <- searchTwitter("USA, Politics, Liberals")
+
+conservative_tweets <- searchTwitter("#Conservatives", n = 100) 
+liberal_tweets <- searchTwitter("#Liberals", n = 100)
+
