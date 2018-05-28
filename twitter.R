356675b428562317071e331a7216a8d2833ec3ed
amjadz <amjadz@uw.edu>
2018-05-27 23:10:31 -0700

    Merged all data

--- twitter.R
+++ twitter.R
@@ -6,13 +6,22 @@ source("api-keys.R")
 
 setup_twitter_oauth(consumer_public, consumer_private, public_key, private_key)
 
+# Grabbed the required 
 conservative_tweets <- searchTwitter("#Conservatives") 
 liberal_tweets <- searchTwitter("#Liberals")
-# Use something like this for data: liberal_df <- twListToDF(liberal_tweets)
-
-
 gun_control_tweets <- searchTwitter("#gun control")
 anti_gun_control_tweets <- searchTwitter("#noguncontrol")
-
 abortion_tweets <- searchTwitter("#abortion")
 anti_abortion_tweets <- searchTwitter("#noabortion")
+
+# Converted to Data Frames
+liberal_df <- twListToDF(liberal_tweets)
+conservative_df <- twListToDF(conservative_tweets)
+gun_control_df <- twListToDF(gun_control_tweets)
+anti_gun_control_df <-twListToDF(anti_gun_control_tweets)
+abortion_df <- twListToDF(abortion_tweets)
+anti_abortion_df <- twListToDF(anti_abortion_tweets)
+
+
+# All data merged
+all_data_merged <- rbind(liberal_df, conservative_df, gun_control_df, anti_gun_control_df, abortion_df, anti_abortion_df)
