2666051ea3ff09cb5dabda08d54fa8aad9099865
sambuxton <37856691+sambuxton@users.noreply.github.com>
2018-05-30 10:26:14 -0700

    Revert thing back to old thing

--- server.R
+++ server.R
@@ -33,56 +33,48 @@ shinyServer(
     output$plot <- renderPlot({
       tweet_info <- searchTwitter(paste0("#", input$hashtag), 50)
       tweet_info_df <- twListToDF(tweet_info)
-      
+
       text <- data_frame(tweet = tweet_info_df$text) %>%
         unnest_tokens(word, tweet)
-      
+
       tweet_score <- text %>%
         inner_join(get_sentiments("afinn"), by = "word") %>%
         count(word, score, sort = TRUE) %>%
         ungroup()
-      
+
       # I split these up for clarity, although they could be combined
       tweet_sentiment <- text %>%
         inner_join(get_sentiments("nrc"), by = "word") %>%
         count(sentiment, sort = TRUE) %>%
         ungroup() %>%
         rename(word = sentiment)
-      
+
       binary_sentiment <- tweet_sentiment %>%
         inner_join(get_sentiments("bing"), by = "word") %>%
         ungroup() %>%
         arrange(n)
-      
-      
-      is.data.frame(binary_sentiment)
+
+
+   is.data.frame(binary_sentiment)   
       total_score <- tweet_score$score * tweet_score$n
-      
-      
+
+
       # I would like to point out that it was NOT easy to get those colors right
-      positivity_plot <- ggplot(data = tweet_score, aes(
-        x = factor(word),
-        y = total_score
-      )) +
+      positivity_plot <- ggplot(data = tweet_score, aes(x = factor(word), y = total_score)) +
         geom_bar(aes(fill = total_score < 0), stat = "identity") +
         theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-        scale_fill_manual(
-          guide = FALSE, breaks = c(TRUE, FALSE),
-          values = c("green", "red")
-        ) +
+        scale_fill_manual(guide = FALSE, breaks = c(TRUE, FALSE), values = c("green", "red")) +
         labs(
           x = "Most Common Words in Tweets",
           y = "Positivity Score",
           title = "Positivity of Tweets",
           fill = "Positivity"
         )
-      
+
       # Using only 3 through 8 since 'positive' and 'negative are by
       # far the most popular sentiments, although they cannot be used
-      
-      sentiment <- ggplot(binary_sentiment, aes(x = factor(word,
-                                                           levels = unique(word)
-      ), y = n)) +
+
+      sentiment <- ggplot(binary_sentiment, aes(x = factor(word, levels = unique(word)), y = n)) +
         geom_bar(stat = "identity", aes(fill = factor(sentiment))) +
         coord_flip() +
         labs(
@@ -91,10 +83,10 @@ shinyServer(
           title = "Emotions Conveyed by Tweets",
           fill = "Positivity"
         )
-      
+
       ggarrange(sentiment, positivity_plot,
-                labels = c("A", "B"),
-                ncol = 2, nrow = 1
+        labels = c("A", "B"),
+        ncol = 2, nrow = 1
       )
     })
     
