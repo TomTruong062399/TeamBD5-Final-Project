6cf000a65373ce2d6af2b2516dcfe3bf97c68eda
amjadz <amjadz@uw.edu>
2018-05-29 20:51:30 -0700

    Done with mine

--- ui.R
+++ ui.R
@@ -4,24 +4,25 @@ library(plotly)
 ui <- shinyUI(
   navbarPage(
     "Project Name Here",
-      tabPanel(
-        "How do People Feel About this Topic?",
-        sidebarPanel("Hashtag",
-                     width = 3,
-                     textInput(inputId = "hashtag", "Which hashtag would you like to look up?")
-        ),
-        mainPanel(
-          plotlyOutput("plot")
-        )
+    tabPanel(
+      "How do People Feel About this Topic?",
+      sidebarPanel("Hashtag",
+        width = 3,
+        textInput(inputId = "hashtag", "Which hashtag would you like to look up?")
       ),
-      tabPanel(
-       "Sample by Term",
+      mainPanel(
+        plotlyOutput("plot")
+      )
+    ),
+    tabPanel(
+      "Sample by Term",
       titlePanel("Sample 100 Tweets/Retweets by Term"),
       sidebarLayout(
         sidebarPanel(
           textInput("term",
-                    label = "Search for a Term",
-                    value = "Trump")
+            label = "Search for a Term",
+            value = "Trump"
+          )
         ),
         mainPanel(
           tableOutput("key_tweets")
@@ -43,29 +44,28 @@ ui <- shinyUI(
       mainPanel(
         tableOutput("scripts/twitter.R")
       )
-    ),  
-      tabPanel(
-        "Most Retweeted",
-        titlePanel("Top 5 Tweets/Retweets by Retweets"),
-        sidebarLayout(
-          sidebarPanel(
-            textInput("top_5",
-              label = "Top 5 Tweets/Retweets from a Random Sample",
-              value = "Trump"
-            ),
-            sliderInput("quantity",
-              label = "Select of Sample Size",
-              min = 1,
-              max = 1000,
-              value = 500,
-              ticks = TRUE,
-              animate = FALSE
-            )
+    ),
+    tabPanel(
+      "Most Retweeted",
+      titlePanel("Top 5 Tweets/Retweets by Retweets"),
+      sidebarLayout(
+        sidebarPanel(
+          textInput("top_5",
+            label = "Top 5 Tweets/Retweets from a Random Sample",
+            value = "Trump"
           ),
-          mainPanel(
-            plotlyOutput("tweets_plot"),
-            tableOutput("tweets_tbl")
+          sliderInput("quantity",
+            label = "Select of Sample Size",
+            min = 1,
+            max = 1000,
+            value = 500,
+            ticks = TRUE,
+            animate = FALSE
           )
+        ),
+        mainPanel(
+          plotlyOutput("tweets_plot"),
+          tableOutput("tweets_tbl")
         )
       )
     )
