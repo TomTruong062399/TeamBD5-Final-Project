fec580191b087175ab7639cee1f94edada923736
Moses Chong <moses.chong42288@gmail.com>
2018-05-29 17:46:34 -0700

    Sampling Tweets/Retweets

--- ui.R
+++ ui.R
@@ -1,22 +1,16 @@
 library(shiny)
+library(plotly)
 
 ui <- shinyUI(navbarPage(
   "Insert Project Name",
     tabPanel(
-      "Key Terms",
-      titlePanel("Key Term Search"),
+      "Sample by Term",
+      titlePanel("Sample 100 Tweets/Retweets by Term"),
       sidebarLayout(
         sidebarPanel(
           textInput("term",
-                    label = "Enter a Term",
-                    value = ""),
-          sliderInput("quantity",
-                      label = "Specify how many tweets",
-                      min = 1,
-                      max = 10,
-                      value = 5,
-                      ticks = TRUE,
-                      animate = FALSE)
+                    label = "Search for a Term",
+                    value = "Trump")
         ),
         mainPanel(
           tableOutput("key_tweets")
@@ -24,11 +18,25 @@ ui <- shinyUI(navbarPage(
       )
     ),
     tabPanel(
-      "Temp",
-      titlePanel("Temporary"),
+      "Most Retweeted",
+      titlePanel("Top 5 Tweets/Retweets by Retweets"),
       sidebarLayout(
-        sidebarPanel("Left Panel"),
-        mainPanel("Right Panel")
+        sidebarPanel(
+          textInput("top_5",
+                    label = "Top 5 Tweets/Retweets from a Random Sample",
+                    value = "Trump"),
+          sliderInput("quantity",
+                      label = "Select of Sample Size",
+                      min = 1,
+                      max = 1000,
+                      value = 500,
+                      ticks = TRUE,
+                      animate = FALSE)
+        ),
+        mainPanel(
+          plotlyOutput("tweets_plot"),
+          tableOutput("tweets_tbl")
+        )
       )
     )
   )
